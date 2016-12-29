4035b32cbc4b1b709c05037b0b761759229c66bb
Francisco Franco <franciscofranco.1990@gmail.com>
2016-12-29 02:13:33 +0000

    display: add a simple api to query the display state (on/off) at any point in time
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -22,6 +22,7 @@
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
 #include <linux/string.h>
+#include <linux/display_state.h>
 
 #include "mdss_dsi.h"
 #ifdef TARGET_HW_MDSS_HDMI
@@ -46,6 +47,12 @@ bool panel_rst_high_flag = 0;
 struct dsi_panel_cmds bist_cmds;
 bool bist_cmds_on = false;
 #endif
+
+bool display_on = true;
+bool is_display_on()
+{
+	return display_on;
+}
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -1062,6 +1069,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
 
+	display_on = false;
+
 end:
 	/* clear idle state */
 	ctrl->idle = false;
@@ -1079,6 +1088,8 @@ static int mdss_dsi_panel_low_power_config(struct mdss_panel_data *pdata,
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
+
+	display_on = true;
 
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
